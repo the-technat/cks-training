726eb33e3e52c16bf2e7799de099a9de1a386821
Nathanael Liechti <technat@technat.ch>
2023-01-27 09:10:07 +0100

    fix: only use one ssh key

--- kubernetes.tf
+++ kubernetes.tf
@@ -6,14 +6,13 @@ module "kubernetes" {
 
   cluster_vpc_cidr      = "10.0.0.0/12"
   cluster_subnet_cidr   = "10.1.0.0/16"
-  default_ssh_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMtnizXrvJRuzTWdMB7yqeH+6g35TZwCgkj6ThaUKMb ansible@example.com", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2VGZkovATxoMI4f2g24/Ca0/9fuZETHNCNbGbKxwsS technat@technat.ch"]
+  default_ssh_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJov21J2pGxwKIhTNPHjEkDy90U8VJBMiAodc2svmnFC cardno:18 187 880"]
   default_ssh_port      = 59245
   default_ssh_user      = "technat"
   enable_server_backups = false
   ip_mode               = "ipv4"
   bootstrap_nodes       = true
 
-  ### Control Plane ###
   master_nodes = [
     {
       name        = "hawk"
@@ -28,7 +27,6 @@ module "kubernetes" {
     }
   ]
 
-  ### Compute Plane ###
   worker_nodes = [
     {
       name        = "minion-0"
