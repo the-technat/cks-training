1ae2327a95ce3b8446360b01258ce9ac44f035ae
Nathanael Liechti <technat@technat.ch>
2023-01-30 07:26:49 +0000

    fix: add second ssh key

--- lab_env/kubernetes.tf
+++ lab_env/kubernetes.tf
@@ -4,7 +4,7 @@ module "kubernetes" {
   cluster_name = "cks_gugus"
   region       = "eu-central"
 
-  default_ssh_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJov21J2pGxwKIhTNPHjEkDy90U8VJBMiAodc2svmnFC cardno:18 187 880"]
+  default_ssh_keys      = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJov21J2pGxwKIhTNPHjEkDy90U8VJBMiAodc2svmnFC cardno:18 187 880", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsg76nv5vl7zZpOVNJq8zPZa56KRMFNMsXoP6OZvi/c ephemeral workspace key"]
   default_ssh_port      = 59245
   default_ssh_user      = "technat"
   enable_server_backups = false
